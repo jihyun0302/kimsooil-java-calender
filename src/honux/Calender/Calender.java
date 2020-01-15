440bf4e03898b54d076cb1b96fd15586acea43cd
star <star@192.168.123.100>
2020-01-16 08:24:45 +0900

    Calender 클래스를 이용하여 달의 최대날 수를 구하는 방법

--- src/honux/Calender/Calender.java
+++ src/honux/Calender/Calender.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Calender {
 	
 	private static final int[] maxDays1 = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	public int maxDaysOfMonth(int month) {
+	public int getMaxDaysOfMonth(int month) {
 		return maxDays1[month-1];
 	}
 	public static void main(String[] args) {
@@ -21,10 +21,11 @@ public class Calender {
 		
 		//숫자를 입력받아 해당하는 달의 최대일수를 출력하는 방법
 		Scanner scanner = new Scanner(System.in);
+		Calender cal = new Calender();
 		System.out.println("달을 입력하세요");
 		int month = scanner.nextInt();
 		
-		System.out.printf("%d월의 최대일수는 %d일 입니다.\n", month, maxDays1[month-1]);
+		System.out.printf("%d월의 최대일수는 %d일 입니다.\n", month, cal.getMaxDaysOfMonth(month));
 		scanner.close();
 	}
 }
