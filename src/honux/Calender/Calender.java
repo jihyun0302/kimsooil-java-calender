574f1e6182dac0e2a2aa3a49df55b0739e2763ae
jihyun <wlwl3681@naver.com>
2020-04-29 02:10:53 +0900

    add year date

--- src/honux/Calender/Calender.java
+++ src/honux/Calender/Calender.java
@@ -1,5 +1,6 @@
 package honux.Calender;
 
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class Calender {
@@ -27,8 +28,49 @@ public class Calender {
 		int month = scanner.nextInt();
 
 		System.out.printf("%d월의 최대일수는 %d일 입니다.\n", month, cal.getMaxDaysOfMonth(month));
+		
+		int y,m;
+		System.out.println("몇년도 달력을 희망하시나요?");
+		y=scanner.nextInt();
+		
+		//요일 확인 일요일 =1,월요일2,화3수4목5
+		for(int j=1;j<=12;j++) {
+			cal.set(y,j-1,1);
+			int w=cal.get(Calendar.DAY_OF_WEEK);
+			int end = cal.getActualMaximum(Calendar.DATE);
+			
+			System.out.println("\t"+y+"년"+j+"월");
+			
+			for (int i=1;i<w;i++) {
+				System.out.print("\t");
+			}
+			for(int i = 1;i<=end;i++) {
+				System.out.print("\t"+i);
+				if(++w%7==1)
+					System.out.println();
+			}
+			System.out.println();
+		}
+		
+
+		
 		scanner.close();
 		
+		
+	}
+
+	private int getActualMaximum(int date) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private int get(int dayOfWeek) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private void set(int y, int i, int j) {
+		// TODO Auto-generated method stub
 		
 	}
 }
