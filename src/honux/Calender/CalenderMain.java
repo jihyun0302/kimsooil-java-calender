4bfdb9b0cf25b082591d8e4e77a606fae4e41ca9
jihyun <wlwl3681@naver.com>
2020-04-29 14:48:37 +0900

    added calendermain

---
+++ src/honux/Calender/CalenderMain.java
@@ -0,0 +1,12 @@
+package honux.Calender;
+
+import java.text.ParseException;
+
+
+public class CalenderMain {
+	public static void main(String[] args) throws ParseException {
+		// ¼¿ ½ÇÇà
+		Prmpt p = new Prmpt();
+		p.runPrompt();
+	}
+}
